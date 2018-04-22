9bad3a485cfbbe8595c11e4a898c603e28be8631
Ballantyne, Bob <bob.ballantyne@emc.com>
2018-04-22 11:24:09 -0400

    update execute attrs

--- devUpdate.sh
+++ devUpdate.sh
@@ -4,6 +4,7 @@ cd ~/support.dev
 docker-compose build base
 docker-compose build openjdk
 docker-compose build supervisord
+docker-compose build php5
 
 cd ~/prod
 docker-compose build broker
@@ -16,5 +17,9 @@ docker-compose build phpadmin
 docker-compose build webdav
 docker-compose build zen
 docker-compose build zookeeper
+docker-compose build smonitor
 docker-compose build kafkamgr
 
+#docker-compose down
+#roc
+#docker-compose up -d
