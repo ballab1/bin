61f2e1af0d1fa22d5649152ff962224f4644b238
Ballantyne, Bob <bob.ballantyne@emc.com>
2018-05-05 17:02:41 -0400

    runtime updates

--- devUpdate.sh
+++ devUpdate.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+declare start=$(tfile=$(mktemp); stat -c %Y "$tfile"; rm "$tfile" )
+export  CONTAINER_TAG="$( date +%Y%m%d )"
+export  CBF_VERSION=dev
+set -o verbose
+
 cd ~/support
 docker-compose build base
 docker-compose build openjdk
 docker-compose build supervisord
+docker-compose build nginx_base
 docker-compose build php5
+docker-compose build php7
 
 cd ~/prod
 docker-compose build broker
@@ -23,3 +30,9 @@ docker-compose build kafkamgr
 #docker-compose down
 #roc
 #docker-compose up -d
+
+set +o verbose
+declare finish=$(tfile=$(mktemp); stat -c %Y "$tfile"; rm "$tfile" )
+declare -i elapsed=$(( finish - start ))
+printf "Time elapsed: %02d:%02d:%02d\n"  $((elapsed / 3600)) $((elapsed % 3600 / 60)) $((elapsed % 60)) 
+
