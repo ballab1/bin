761ed12215cb9df1a108a87ce795ee24326df851
Ballantyne, Bob <ballantyne.robert@gmail.com>
2024-11-04 14:13:45 -0500

    add helm completion

--- test/tst-synology.sh
+++ test/tst-synology.sh
@@ -2,27 +2,158 @@
 
 
 START="$(date '+%s')"
+
+declare -r NAMESPACE='synology'
+declare -r CHART_NAME='synology-csi/synology-csi'
+declare -r RELEASE_VERIONS='0.10.1'
+declare -r RELEASE_NAME='synology-csi'
+declare -r HELM_VALUES='synology-helm-values.yml'
+declare -r TEST_DEFN_DIR='postgresql'
+declare -r PV_DEFINITION="$TEST_DEFN_DIR/40.PersistentVolume.yml"
+declare -r TEST_DEFN_NS='postgres'
+declare -r RESULTS_FILE='results.log'
+declare -r ERRORS_FILE='errors.log'
 declare -i MAX_ITER=100
-declare RESULTS_LOG='results.log'
+
+declare -a LOCATIONS=( 'K8S'
+                       '/K8S'
+                       'volume1'
+                       '/volume1'
+                       'volume1/K8S'
+                       '/volume1/K8S'
+                       '10.3.1.4'
+                       '//10.3.1.4'
+                       '//10.3.1.4/K8S'
+                       '//10.3.1.4/volume1K8S'
+                     )
+
 declare -a VARIABLES=( 'K8S K8S'
+                       'K8S /K8S'
                        'K8S volume1'
                        'K8S /volume1'
                        'K8S /volume1/K8S'
+                       'K8S 10.3.1.4/K8S'
+                       'K8S //10.3.1.4/K8S'
+                       'K8S //10.3.1.4/volume1/K8S'
+                       '/K8S K8S'
                        '/K8S /K8S'
                        '/K8S volume1'
                        '/K8S /volume1'
                        '/K8S /volume1/K8S'
+                       '/K8S 10.3.1.4/K8S'
+                       '/K8S //10.3.1.4/K8S'
+                       '/K8S //10.3.1.4/volume1/K8S'
                        'volume1 K8S'
                        'volume1 /K8S'
                        'volume1 /volume1/K8S'
+                       'volume1 10.3.1.4/K8S'
+                       'volume1 //10.3.1.4/K8S'
+                       'volume1 //10.3.1.4/volume1/K8S'
                        '/volume1 K8S'
                        '/volume1 /K8S'
                        '/volume1 /volume1/K8S'
-                       '/volume1/K8S volume1'
-                       '/volume1/K8S /volume1'
+                       '/volume1 10.3.1.4/K8S'
+                       '/volume1 //10.3.1.4/K8S'
+                       '/volume1 //10.3.1.4/volume1/K8S'
                        '/volume1/K8S K8S'
                        '/volume1/K8S /K8S'
+                       '/volume1/K8S volume1'
+                       '/volume1/K8S /volume1'
                        '/volume1/K8S /volume1/K8S'
+                       '/volume1/K8S 10.3.1.4/K8S'
+                       '/volume1/K8S //10.3.1.4/K8S'
+                       '/volume1/K8S 10.3.1.4/volume1'
+                       '/volume1/K8S //10.3.1.4/volume1'
+                       '/volume1/K8S 10.3.1.4/volume1/K8S'
+                       '/volume1/K8S //10.3.1.4/volume1/K8S'
+                       '10.3.1.4 K8S'
+                       '10.3.1.4 /K8S'
+                       '10.3.1.4 volume1'
+                       '10.3.1.4 /volume1'
+                       '10.3.1.4 /volume1/K8S'
+                       '10.3.1.4 10.3.1.4/K8S'
+                       '10.3.1.4 //10.3.1.4/K8S'
+                       '10.3.1.4 10.3.1.4/volume1'
+                       '10.3.1.4 //10.3.1.4/volume1'
+                       '10.3.1.4 10.3.1.4/volume1/K8S'
+                       '10.3.1.4 //10.3.1.4/volume1/K8S'
+                       '//10.3.1.4 K8S'
+                       '//10.3.1.4 /K8S'
+                       '//10.3.1.4 volume1'
+                       '//10.3.1.4 /volume1'
+                       '//10.3.1.4 /volume1/K8S'
+                       '//10.3.1.4 10.3.1.4/K8S'
+                       '//10.3.1.4 //10.3.1.4/K8S'
+                       '//10.3.1.4 10.3.1.4/volume1'
+                       '//10.3.1.4 //10.3.1.4/volume1'
+                       '//10.3.1.4 10.3.1.4/volume1/K8S'
+                       '//10.3.1.4 //10.3.1.4/volume1/K8S'
+                       '10.3.1.4/K8S K8S'
+                       '10.3.1.4/K8S /K8S'
+                       '10.3.1.4/K8S volume1'
+                       '10.3.1.4/K8S /volume1'
+                       '10.3.1.4/K8S /volume1/K8S'
+                       '10.3.1.4/K8S 10.3.1.4/K8S'
+                       '10.3.1.4/K8S //10.3.1.4/K8S'
+                       '10.3.1.4/K8S 10.3.1.4/volume1'
+                       '10.3.1.4/K8S //10.3.1.4/volume1'
+                       '10.3.1.4/K8S 10.3.1.4/volume1/K8S'
+                       '10.3.1.4/K8S //10.3.1.4/volume1/K8S'
+                       '10.3.1.4/volume1 K8S'
+                       '10.3.1.4/volume1 /K8S'
+                       '10.3.1.4/volume1 volume1'
+                       '10.3.1.4/volume1 /volume1'
+                       '10.3.1.4/volume1 /volume1/K8S'
+                       '10.3.1.4/volume1 10.3.1.4/K8S'
+                       '10.3.1.4/volume1 //10.3.1.4/K8S'
+                       '10.3.1.4/volume1 10.3.1.4/volume1'
+                       '10.3.1.4/volume1 //10.3.1.4/volume1'
+                       '10.3.1.4/volume1 10.3.1.4/volume1/K8S'
+                       '10.3.1.4/volume1 //10.3.1.4/volume1/K8S'
+                       '10.3.1.4/volume1/K8S K8S'
+                       '10.3.1.4/volume1/K8S /K8S'
+                       '10.3.1.4/volume1/K8S volume1'
+                       '10.3.1.4/volume1/K8S /volume1'
+                       '10.3.1.4/volume1/K8S /volume1/K8S'
+                       '10.3.1.4/volume1/K8S 10.3.1.4/K8S'
+                       '10.3.1.4/volume1/K8S //10.3.1.4/K8S'
+                       '10.3.1.4/volume1/K8S 10.3.1.4/volume1'
+                       '10.3.1.4/volume1/K8S //10.3.1.4/volume1'
+                       '10.3.1.4/volume1/K8S 10.3.1.4/volume1/K8S'
+                       '10.3.1.4/volume1/K8S //10.3.1.4/volume1/K8S'
+                       '//10.3.1.4/K8S K8S'
+                       '//10.3.1.4/K8S /K8S'
+                       '//10.3.1.4/K8S volume1'
+                       '//10.3.1.4/K8S /volume1'
+                       '//10.3.1.4/K8S /volume1/K8S'
+                       '//10.3.1.4/K8S 10.3.1.4/K8S'
+                       '//10.3.1.4/K8S //10.3.1.4/K8S'
+                       '//10.3.1.4/K8S 10.3.1.4/volume1'
+                       '//10.3.1.4/K8S //10.3.1.4/volume1'
+                       '//10.3.1.4/K8S 10.3.1.4/volume1/K8S'
+                       '//10.3.1.4/K8S //10.3.1.4/volume1/K8S'
+                       '//10.3.1.4/volume1 K8S'
+                       '//10.3.1.4/volume1 /K8S'
+                       '//10.3.1.4/volume1 volume1'
+                       '//10.3.1.4/volume1 /volume1'
+                       '//10.3.1.4/volume1 /volume1/K8S'
+                       '//10.3.1.4/volume1 10.3.1.4/K8S'
+                       '//10.3.1.4/volume1 //10.3.1.4/K8S'
+                       '//10.3.1.4/volume1 10.3.1.4/volume1'
+                       '//10.3.1.4/volume1 //10.3.1.4/volume1'
+                       '//10.3.1.4/volume1 10.3.1.4/volume1/K8S'
+                       '//10.3.1.4/volume1 //10.3.1.4/volume1/K8S'
+                       '//10.3.1.4/volume1/K8S K8S'
+                       '//10.3.1.4/volume1/K8S /K8S'
+                       '//10.3.1.4/volume1/K8S volume1'
+                       '//10.3.1.4/volume1/K8S /volume1'
+                       '//10.3.1.4/volume1/K8S /volume1/K8S'
+                       '//10.3.1.4/volume1/K8S 10.3.1.4/K8S'
+                       '//10.3.1.4/volume1/K8S //10.3.1.4/K8S'
+                       '//10.3.1.4/volume1/K8S 10.3.1.4/volume1'
+                       '//10.3.1.4/volume1/K8S //10.3.1.4/volume1'
+                       '//10.3.1.4/volume1/K8S 10.3.1.4/volume1/K8S'
+                       '//10.3.1.4/volume1/K8S //10.3.1.4/volume1/K8S'
 )
 
 # ---------------------------------------------------------------------------
@@ -35,17 +166,18 @@ function fmtElapsedTime()
 
 # ---------------------------------------------------------------------------
 function main() {
+  local location
   local -i ID=0
-  local -i begin last
+
   begin="$START"
-  [ -f "$RESULTS_LOG" ] && rm "$RESULTS_LOG"
-  for params in "${VARIABLES[@]}"; do
-    #shellcheck disable=SC2086
-    result="$(test $params)"
-    last="$(date '+%s')"
-    printf '%s: id: %s, %s\n' "$(fmtElapsedTime "$((last-begin))")" "$(( ID++ ))" "$result"
-    begin="$last"
-  done > "$RESULTS_LOG"
+  cd ~/workspace || :
+  [ -f "$ERRORS_FILE" ] && :> "$ERRORS_FILE"
+  [ -f "$RESULTS_FILE" ] && rm "$RESULTS_FILE"
+  [ "$(kubectl get namespace -o json | jq -r '[.items[]|select(.metadata.name == "'"$NAMESPACE"'")]|length')" -eq 0 ] && kubectl create namespace "$NAMESPACE" > /dev/null
+
+  for location in "${LOCATIONS[@]}"; do
+    test_variables "$location"
+  done
 }
 
 # ---------------------------------------------------------------------------
@@ -53,7 +185,7 @@ function node_start() {
 
   local -r pod="${1:?}"
 
-  LAST="$(kubectl logs -n synology -c csi-plugin --tail 1 "${pod}" | cut -d ' ' -f 1)"
+  LAST="$(kubectl logs -n "$NAMESPACE" -c csi-plugin --tail 1 "${pod}" | cut -d ' ' -f 1)"
   if [ "${#LAST}" -gt 0 ];then
      local -i SECS="$(date '+%s' --date="$LAST")"
      date "--date=@$(( SECS + 1 ))" '+%Y-%m-%dT%T%:z'
@@ -70,16 +202,18 @@ function node_time() {
   local LINES=''
   local -i COUNT=0
   while true; do
-    LINES="$(kubectl logs -n synology -c csi-plugin --since-time="${start}" "$POD" | grep ' \[ERROR\] ')"
+    LINES="$(kubectl logs -n "$NAMESPACE" -c csi-plugin --since-time="${start}" "$POD" | grep ' \[ERROR\] ')"
     if [ "${#LINES}" -gt 20 ] && [ "$(wc -l <<< "$LINES")" -gt 0 ]; then
       # return time of last error found
       echo "$COUNT"
       tail -1 <<< "$LINES" | cut -d ' ' -f 1
+      printf '%s: %s\n' "$ID" "$LINES" >> "$ERRORS_FILE"
       return
     fi
     [ "$(( ++COUNT ))" -ge "$MAX_ITER" ] && break
     sleep 2
   done
+  printf '%s:\n' "$ID" >> "$ERRORS_FILE"
   echo "$COUNT"
   echo '---------------'
 }
@@ -87,9 +221,9 @@ function node_time() {
 # ---------------------------------------------------------------------------
 function onExit() {
   trap - EXIT
-  if [ -f "$RESULTS_LOG" ]; then
+  if [ -f "$RESULTS_FILE" ]; then
     echo
-    cat "$RESULTS_LOG"
+    cat "$RESULTS_FILE"
   fi
   local -i last=""
   local -i elapsed="$(( $(date '+%s') - START ))"
@@ -102,18 +236,19 @@ function run_test() {
   local NODE=''
   local -i COUNT=0
   while [ "${#NODE}" -eq 0 ];do
-    NODE="$(kubectl get pod -n postgres -o 'jsonpath={.items[0].spec.nodeName}' 2>/dev/null)"
+    NODE="$(kubectl get pod -n "$TEST_DEFN_NS" -o 'jsonpath={.items[0].spec.nodeName}' 2>/dev/null)"
     [ "${#NODE}" -gt 0 ] && break
     [ "$(( ++COUNT ))" -ge "$MAX_ITER" ] && break
     sleep 2
   done
   if [ "${#NODE}" -eq 0 ]; then
-      printf 'id: %s, source: %s, volumeHandle: %s, result: postgres node not found\n' "$id" "$source" "$volume"
+      printf 'id: %s, source: %s, volumeHandle: %s, result: %s node not found\n' "$ID" "$source" "$volume" "$TEST_DEFN_NS"
   else
     # get the associated start time (now we know the node)
     local -r POD="${POD_MAP[$NODE]}"
     if [ "${#POD}" -gt 0 ]; then
       # print the test parameters being tested and result
+      #shellcheck disable=SC2183,SC2046
       printf 'source: %s, volumeHandle: %s, count: %s, result: %s' "$source" "$volume" $(node_time "$POD" "${POD_TIMES[$NODE]}")
     fi
   fi
@@ -129,10 +264,10 @@ function test() {
   sed -i -E \
       -e 's|^(\s+source: ).*$|\1'"$source"'|' \
       -e 's|^(\s+volumeHandle: ).*$|\1'"$volume"'|' \
-      'postgresql/40.PersistentVolume.yml'
+      "$PV_DEFINITION"
 
   # get the pod id's running on each node (use node as index)
-  local -r JQ_RSLT="$(kubectl get pod -n synology -o json | jq -r '.items[]|select(.metadata.labels.app == "node")|"[\(.spec.nodeName)]=\(.metadata.name)"' )"
+  local -r JQ_RSLT="$(kubectl get pod -n "$NAMESPACE" -o json | jq -r '.items[]|select(.metadata.labels.app == "node")|"[\(.spec.nodeName)]=\(.metadata.name)"' )"
   local -A POD_MAP
   eval "POD_MAP=( $JQ_RSLT )"
 
@@ -143,10 +278,34 @@ function test() {
   done
 
   # configure PV & PVC to use our test parameters
-  kubectl create -f postgresql > /dev/null
-  yaml2json postgresql/40.PersistentVolume.yml | jq -c '.spec.csi|{id:'"${ID}"', source: ."volumeAttributes".source, "volumeHandle"}' >&2
+  [ "$(kubectl get namespace -o json | jq -r '[.items[]|select(.metadata.name == "'"$TEST_DEFN_NS"'")]|length')" -gt 0 ] && kubectl delete -f "$TEST_DEFN_DIR" > /dev/null
+  kubectl create -f "$TEST_DEFN_DIR" > /dev/null
+  yaml2json "$PV_DEFINITION" | jq -c '.spec.csi|{id:'"${ID}"', source: ."volumeAttributes".source, "volumeHandle"}' >&2
   run_test
-  kubectl delete -f postgresql > /dev/null
+  kubectl delete -f "$TEST_DEFN_DIR" > /dev/null
+}
+
+# ---------------------------------------------------------------------------
+function test_variables() {
+  local -r location="${1:?}"
+  local -i begin last
+  local params
+
+
+  [ "$(helm list -n "$NAMESPACE" -o json | jq 'length')" -gt 0 ] && helm uninstall -n "$NAMESPACE" "$RELEASE_NAME"
+  # update helm values
+  sed -i -E \
+      -e 's|^(\s+location: ).*$|\1'"$location"'|g' \
+      "$HELM_VALUES"
+  helm install -n "$NAMESPACE" -f "$HELM_VALUES" --version "$RELEASE_VERIONS" "$RELEASE_NAME" "$CHART_NAME" | head -5
+
+  for params in "${VARIABLES[@]}"; do
+    #shellcheck disable=SC2086
+    result="$(test $params)"
+    last="$(date '+%s')"
+    printf '%s: id: %s, location: %s, %s\n' "$(fmtElapsedTime "$((last-begin))")" "$(( ID++ ))" "$location" "$result"
+    begin="$last"
+  done >> "$RESULTS_FILE"
 }
 
 # ---------------------------------------------------------------------------
