d2f4b8c4f372594890ec1e0f06d2f45be8522ebe
Ballantyne, Bob <ballantyne.robert@gmail.com>
2025-01-13 18:13:35 -0500

    updates to capture.sh

--- capture.sh
+++ capture.sh
@@ -6,16 +6,18 @@ kill_tcpdump() {
 }
 
 rm_file() {
-    [ ! -e  "$DUMP_FILE" ] || sudo rm "$DUMP_FILE"
+    sudo find ~/wireshark -name '*.pcap' -delete
 }
 
 start_tcpdump() {
-    IFACE="$(ip -4 -j address show scope global dynamic|jq -r '.[0].addr_info[].label')"
+    IFACE="$(ip -4 -j address|jq -r '.[]|select(.qdisc != "noqueue").addr_info[0].label')"
+    #IFACE="$(ip -4 -j address|jq -r '.[]|select(.qdisc == "fq_codel").addr_info[0].label')"
+    #IFACE="$(ip -4 -j address show scope global dynamic|jq -r '.[0].addr_info[].label')"
     [ -f 'sysfile.log' ] && sudo rm sysfile.log 
-    sudo SSLKEYLOGFILE=./keyfile.log tcpdump -w "$DUMP_FILE" -i "$IFACE" $@ &
+    sudo SSLKEYLOGFILE=./keyfile.log tcpdump -i $IFACE -w "$DUMP_FILE" -i "$IFACE" -W 24 -G 3600 --relinquish-privileges=bobb $@ &
 }
 
-DUMP_FILE=~/wireshark/tcp.dump
+DUMP_FILE=~/wireshark/capture_%Y-%m-%d_%H-%M-%S.pcap
 DC_FILE=~/wireshark/docker-compose.yml
 MODE="${1:?'must specify an argument from start|stop|up|down'}"
 [ "$#" -gt 0 ] || exit 1
