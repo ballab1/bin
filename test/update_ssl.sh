bcdcd02a8af553a8ef41f492032166f8d19f49d3
Ballantyne, Bob <ballantyne.robert@gmail.com>
2025-06-13 13:23:18 -0400

    add test/update_ssl.sh

--- test/update_ssl.sh
+++ test/update_ssl.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
+if [ ! -f ~/GIT/Soho-Ball.certs/Soho-Ball_CA/certs.tgz ]; then
+    echo 'unable to find ~/GIT/Soho-Ball.certs/Soho-Ball_CA/certs.tgz'
+    ls -l ~/GIT/Soho-Ball.certs/Soho-Ball_CA/
+    exit
+fi
+
+while read -r f; do
+    sudo rm "/etc/ssl/certs/$f"
+done < <(ls -l /etc/ssl/certs/ | grep 'Ball' | cut -d ' ' -f 13)
+sudo find /usr/local/share/ca-certificates/soho-ball/ -type f -delete
+sudo update-ca-certificates
+
+find ~/.inf/ssl -type f -delete
+find -mindepth 1 -maxdepth 1 ~/GIT/support/logs/ -type f -delete
+find ~/production/workspace.production/.secrets -delete
+[ -f ~/Downloads/certs.tgz ] && rm ~/Downloads/certs.tgz
+
+cd ~/.inf/ssl
+tar xvzf ~/GIT/Soho-Ball.certs/Soho-Ball_CA/certs.tgz
+sudo cp SohoBall_CA.crt /usr/local/share/ca-certificates/soho-ball/
+cp ~/GIT/Soho-Ball.certs/Soho-Ball_CA/certs.tgz ~/Downloads/
+cd /usr/local/share/ca-certificates/soho-ball/
+sudo update-ca-certificates
+
 cd ~/production
 deploy down
-rm -rf workspace.production/.secrets
-cd ~/.inf/ssl
-tar cvzf ~/GIT/Soho-Ball.certs/Soho-Ball_CA/certs.tgz *
+
 cd ~/GIT/support
-rm logs/*
 build.sh -f jenkins
 docker system prune -f -a
+
 cd ~/production
-rm -rf workspace.production/.secrets/
 CONTAINER_TAG=dev deploy
 
